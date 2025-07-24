b6a1037382cf0309f89e4e369ffa82204fa15caf
Raghav Aloria <raghavaloria@gmail.com>
2025-07-24 23:35:12 +0530

    ways to reach top with matrix exponentiation

--- Dynamic Programming/WaysToReachNthStairs.cpp
+++ Dynamic Programming/WaysToReachNthStairs.cpp
@@ -22,10 +22,43 @@ int countWays(int n) {
     
 }
 
+void multiplyMat(vector<vector<int>>& a , vector<vector<int>>& b ) { 
+    vector<vector<int>> res(2,vector<int>(2) ) ;
+    res[0][0] = a[0][0] * b[0][0] + a[0][1] * b[1][0];
+    res[0][1] = a[0][0] * b[0][1] + a[0][1] * b[1][1];
+    res[1][0] = a[1][0] * b[0][0] + a[1][1] * b[1][0];
+    res[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1];
+
+    a[0][0] = res[0][0];
+    a[1][0] = res[1][0];
+    a[0][1] = res[0][1];
+    a[1][1] = res[1][1];
+}
+
+vector<vector<int>> power(vector<vector<int>>& m , int expo) { 
+    vector<vector<int>> res = {{1,0}, {0,1}} ;
+    while ( expo ) { 
+        if ( expo & 1 )
+            multiplyMat(res , m) ; 
+        multiplyMat(m , m ) ; 
+        expo >>= 1 ;  
+    }
+    return res ; 
+}
+int countWaysMatrixExpo( int  n ) { 
+    if ( n == 0 || n == 1) return 1 ; 
+
+    vector<vector<int>> m = {{1,1} , {1,0}} ; 
+    vector<vector<int>> initialMatrix = {{1,1} , {1,0}} ; 
+    vector<vector<int>> res = power(m , n -1 ); 
+    multiplyMat(res , initialMatrix);
+    return res[0][0] ; 
+}
+
 int main() {
     vector<int> expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
     for (int n = 0; n <= 10; ++n) {
-        int result = countWays(n);
+        int result = countWaysMatrixExpo(n);
         cout << "countWays(" << n << ") = " << result << endl;
         assert(result == expected[n]);
     }
